37bab3d26aae5b8436213811fc5df267dcdb6cca
Marceloni Lima <markill123@gmail.com>
2019-09-03 21:15:28 -0300

    Updates

---
+++ precache-manifest.86dcb93d7bd2af35fb135a865fb00ed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0c8f6e30caf4370cc18dc1fdd5bbbc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "36f9083d8d65513a4e93",
+    "url": "/monsters-rolodex/static/css/main.6ed5c6fb.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "36f9083d8d65513a4e93",
+    "url": "/monsters-rolodex/static/js/main.1b7b4c17.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
